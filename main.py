1bc031b79fd33f02f02efbd06acd757a64483625
hybridgreen <y.yayaoye@gmail.com>
2025-05-25 16:05:12 +0900

    -Grouped sprites by Updatables and drawables /n -Improved documentation by adding comments

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ import player
 def main():
     print("Starting Asteroids!")
     pygame.init() #Initialise all pygame modules
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) #Setup game screen object
+
+     # Create game environment. Screen and clock control FPS
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clk = pygame.time.Clock()
     dt = 0
+
+     # Create object groups to facilitate updates sprites
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    player.Player.containers = (updatables, drawables)
+
+     #Initialise individual spritesg
     p1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+     #Main game loop to imprement game funtionality
     while(True):
         screen.fill("black")
-        p1.update(dt) 
-        p1.draw(screen) # render the player
+        updatables.update(dt) 
+        for sprite in drawables:
+             sprite.draw(screen) 
+
         for event in pygame.event.get(): # Implementing the exit button to close the game window
              if event.type == pygame.QUIT:
                   return
