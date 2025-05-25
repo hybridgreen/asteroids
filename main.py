f6b78638229ef69e26b08038e01ab63a91745ce4
hybridgreen <y.yayaoye@gmail.com>
2025-05-25 14:27:38 +0900

    CH2 L3 : Set game fps and Delta T

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    pygame.init() #Initialise all pygame modules
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) #Setup game screen object
+    clk = pygame.time.Clock()
+    dt = 0
     while(True):
         screen.fill("black")
-        for event in pygame.event.get():
+        for event in pygame.event.get(): # Implementing the exit button to close the game window
              if event.type == pygame.QUIT:
                   return
-        pygame.display.flip()
+        pygame.display.flip() #refresh the screen
+        dt = clk.tick(60)/1000
+
 
 
 if __name__ == "__main__":
