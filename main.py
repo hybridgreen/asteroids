3ac3c6f0ad8dfa755e98731e2867f7c6768f51f4
hybridgreen <y.yayaoye@gmail.com>
2025-05-25 15:14:50 +0900

    CH2 L5: Expanded the circle shape class to create a triangular player sprite

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 #imports 
 import pygame # type: ignore
 from constants import *
+import player
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init() #Initialise all pygame modules
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) #Setup game screen object
     clk = pygame.time.Clock()
     dt = 0
+    p1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while(True):
         screen.fill("black")
+        p1.draw(screen)
+
         for event in pygame.event.get(): # Implementing the exit button to close the game window
              if event.type == pygame.QUIT:
                   return
         pygame.display.flip() #refresh the screen
         dt = clk.tick(60)/1000
-
 
 
 if __name__ == "__main__":
